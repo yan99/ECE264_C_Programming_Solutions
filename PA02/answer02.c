e7231f2b72a4b9b4cda513e39a991dbaf13392cb
yan99 <yan99@purdue.edu>
2014-09-06 23:51:55 -0400

    start homework

--- PA02/answer02.c
+++ PA02/answer02.c
@@ -5,8 +5,10 @@ char main()
 {
   const char * s = "Hello World";
 //  printf("%d\n", my_strlen(s));
-  char h ='H';
+  char h ='\0';
   printf("%d\n", my_countchar(s, h));
+  printf("'%s'\n", my_strchr(s, h));
+  printf("'%s'\n", my_strrchr(s, h));
 }
 
 size_t my_strlen(const char * str)
@@ -24,7 +26,7 @@ int my_countchar(const char * str, char ch)
   int i;
   int len = my_strlen(str);
   int count = 0;
-  for(i=0; i<len; i++)
+  for(i=0; i<=len; i++)
   {
     if(str[i] == ch)
     {
@@ -34,8 +36,59 @@ int my_countchar(const char * str, char ch)
   return count;
 }
 
-char * my_strchr(const char * str, int ch);
+char * my_strchr(const char * str, int ch)
 {
-    
+  char * s;
+  int i = 0;
+  int len = 0;
+  int count = my_countchar(str, ch);
+  if(count != 0)
+  {
+    while(str[len] != ch)
+    {
+      len++;
+    }
+    while(str[len] != '\0')
+    {
+      s[i] = str[len];
+      i++;
+      len++;
+    }
+  }
+  else
+  {
+    s = "NULL";
+  }
+  return s;
 }
 
+char * my_strrchr(const char * str, int ch)
+{
+  char * s;
+  int i = 0;
+  int len = my_strlen(str);
+  int count = my_countchar(str, ch);
+  if(count != 0)
+  {
+    while(str[len] != ch)
+    {
+      len--;
+    }
+    while(str[len] != '\0')
+    {
+      s[i] = str[len];
+      i++;
+      len++;
+    }
+  }
+  else
+  {
+    s = "NULL";
+  }
+  return s;
+}
+/*
+char * my_strstr(const char * haystack, const char * needle)
+{
+}
+*/
